8a9cbd81d189355d92259d822d98fd03f2aabd4b
Elon <99810813+kollderr@users.noreply.github.com>
2025-10-22 14:11:20 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.setText("Выберите фразу");
 tg.MainButton.color = "#2cab37";
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.hide();
 
 let selectedPhrase = "";
 
-// Обработчики для всех кнопок
-document.getElementById("btn1").addEventListener("click", function() {
-    selectedPhrase = "1";
-    tg.MainButton.setText("Отправить фразу из 'Кавказской пленницы'");
+// Функция для обработки выбора фразы
+function selectPhrase(phraseId, buttonText) {
+    selectedPhrase = phraseId;
+    tg.MainButton.setText(`Отправить: ${buttonText}`);
     tg.MainButton.show();
+}
+
+// Назначаем обработчики для всех кнопок
+document.getElementById("btn1").addEventListener("click", function() {
+    selectPhrase("1", "Кавказская пленница");
 });
 
 document.getElementById("btn2").addEventListener("click", function() {
-    selectedPhrase = "2";
-    tg.MainButton.setText("Отправить фразу из 'Иван Васильевич...'");
-    tg.MainButton.show();
+    selectPhrase("2", "Иван Васильевич");
 });
 
 document.getElementById("btn3").addEventListener("click", function() {
-    selectedPhrase = "3";
-    tg.MainButton.setText("Отправить фразу из 'Джентльмены удачи'");
-    tg.MainButton.show();
+    selectPhrase("3", "Джентльмены удачи");
 });
 
 document.getElementById("btn4").addEventListener("click", function() {
-    selectedPhrase = "4";
-    tg.MainButton.setText("Отправить фразу из 'Операция Ы'");
-    tg.MainButton.show();
+    selectPhrase("4", "Операция Ы");
 });
 
 document.getElementById("btn5").addEventListener("click", function() {
-    selectedPhrase = "5";
-    tg.MainButton.setText("Отправить фразу из 'Бриллиантовая рука'");
-    tg.MainButton.show();
+    selectPhrase("5", "Бриллиантовая рука");
 });
 
 document.getElementById("btn6").addEventListener("click", function() {
-    selectedPhrase = "6";
-    tg.MainButton.setText("Отправить фразу из 'Служебный роман'");
-    tg.MainButton.show();
+    selectPhrase("6", "Служебный роман");
 });
 
 // Обработчик главной кнопки
-Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    tg.sendData(selectedPhrase);
-    tg.close();
+tg.MainButton.onClick(function() {
+    if (selectedPhrase) {
+        console.log("Sending data:", selectedPhrase);
+        tg.sendData(selectedPhrase);
+        // Не закрываем веб-приложение сразу, чтобы увидеть результат
+    }
 });
